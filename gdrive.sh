3df67627939a7a04ac87963caf163e7b857502ce
ktz51tz <ktz51tz@gmail.com>
2017-08-25 19:52:14 -0300

    Fri Aug 25 19:52:14 BRT 2017

--- gdrive.sh
+++ gdrive.sh
@@ -1,9 +1,11 @@
 cd ~/grive
 grive sync
-x="Dump`date +%Y%m%d`.sql"
-tm=$(ls --full-time | egrep "`date +%Y-%m-%d`" | cut -d' ' -f 9)
-if [ $x == $tm ];then
+#x="Dump`date +%Y%m%d`.sql
+#tm=$(ls --full-time | egrep "`date +%Y-%m-%d` | cut -d'  -f 9)
+tm=$(ls | egrep "`date +%Y%m%d`")
+#if [ $x == $tm ] ;then
 mysql -u root < $tm
-else
-echo not found
-fi
+cp -f $tm /projects/source/sql 
+#else
+#echo not found
+#fi
