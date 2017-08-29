6f5dc34aa741e59fa2531ee47d5e570e8962be15
ktz51tz <ktz51tz@gmail.com>
2017-08-29 13:57:10 -0300

    Tue Aug 29 13:57:10 BRT 2017

--- gdrive.sh
+++ gdrive.sh
@@ -1,6 +1,6 @@
 cd ~/grive
 grive sync
-#x="Dump`date +%Y%m%d`.sql
+#x="Dump`date +%Y%m%d`.dsql
 #tm=$(ls --full-time | egrep "`date +%Y-%m-%d` | cut -d'  -f 9)
 tm=$(ls | egrep "`date +%Y%m%d`")
 #if [ $x == $tm ] ;then
